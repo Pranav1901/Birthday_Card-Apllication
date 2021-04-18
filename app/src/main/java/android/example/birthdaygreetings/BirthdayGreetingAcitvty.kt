830a38f748a81855723f7692b93786433fe2a8bb
Pranav Rodge <rodgepranav@gmail.com>
2021-04-18 12:48:12 +0530

    intial commit

---
+++ app/src/main/java/android/example/birthdaygreetings/BirthdayGreetingAcitvty.kt
@@ -0,0 +1,23 @@
+package android.example.birthdaygreetings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+
+class BirthdayGreetingAcitvty : AppCompatActivity() {
+
+
+    companion object{
+        const val  NAME_EXTRA ="name_extra"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting_acitvty)
+
+        val name = intent.getStringExtra(NAME_EXTRA)
+        val birthdayGreeting : TextView =findViewById(R.id.birthdayGreeting)
+        birthdayGreeting.text ="Happy Birthday\n $name"
+    }
+}
---
+++ app/src/main/java/android/example/birthdaygreetings/MainActivity.kt
@@ -0,0 +1,25 @@
+package android.example.birthdaygreetings
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun Create_Card(view: View) {
+        val nameInput : EditText =findViewById(R.id.nameInput)
+        val name = nameInput.text.toString()
+//        Toast.makeText(this,"Hello $name",Toast.LENGTH_LONG).show()
+        val intent = Intent(this,BirthdayGreetingAcitvty::class.java)
+        intent.putExtra(BirthdayGreetingAcitvty.NAME_EXTRA,name)
+        startActivity(intent)
+
+    }
+}
